0d7b748736500441eba0bca0b472d07301e76bbf
hailey-pham <haileypham@gmail.com>
2024-12-03 19:29:46 -0500

    update config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/hailey-pham.github.io/",
   plugins: [
     vue(),
     vueDevTools(),
@@ -14,5 +15,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
+  }
 })
